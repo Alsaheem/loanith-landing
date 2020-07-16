8c87761812239ff618c106b22ecb4a3188e556bd
Adebisi Ayomide <Adebisiayomide07@gmail.com>
2020-07-16 22:40:35 +0100

    git push

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+import './Layout.css'
 
 export default function Layout(props:any) {
     return (
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="">
-      <Layout>Home</Layout>
+      <Layout>
+        <div className=" hero_section bg_loanith">
+          <div className="container pt-5">
+            <div className="row pt-5">
+              <div className="col-5 text-white">
+                <h1>There is a beter way to get funds for family and busiess needs</h1>
+                <br/>
+                <p className="lead">Download the mobile from your favourite App Store to get started</p>
+                <br/>
+
+                <div className="d-flex justify-content-between">
+                  <div className=""><img src="./assets/appBadge.png"  loading="lazy" /></div>
+                  <div className=""><img src="./assets/googleBadge.png"  loading="lazy" /></div>
+                </div>
+              </div>
+              <div className="col-2"></div>
+              <div className="col-5">
+                <img src="./assets/main-logo.png" alt=""/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
     </div>
   );
 };
