7d1796380c23ac288059937891e1f8b61dd68acf
adujoseph <iamjosephadu@gmail.com>
2020-07-17 12:21:08 +0100

    contact and support screens

--- src/components/pages/support/Support.tsx
+++ src/components/pages/support/Support.tsx
@@ -1,5 +1,9 @@
-import React, { Component } from 'react'
-import './Support.css';
+import React, { Component } from 'react';
+import {
+    Layout, Input, Button, Collapse, Typography,  Select
+} from 'antd';
+import './Support.css'
+
 
 interface Props {
     
@@ -8,12 +12,63 @@ interface State {
     
 }
 
+
+const { Option } = Select;
+const { Content } = Layout;
+const { Text } = Typography;
+const { TextArea } = Input;
+const { Panel } = Collapse;
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
 const Support = () => {
+
+
     return (
-        <div>
-            <h4>Support Page</h4>
-        </div>
+        <Layout>
+            <Content >
+                <div>
+                    <h3>Frequently Asked Questions</h3>
+                </div>
+                <div>
+                    <div>
+                        <Collapse accordion>
+                            <Panel header="This is panel header 1" key="1">
+                                <p>{text}</p>
+                            </Panel>
+                            <Panel header="This is panel header 2" key="2">
+                                <p>{text}</p>
+                            </Panel>
+                            <Panel header="This is panel header 3" key="3">
+                                <p>{text}</p>
+                            </Panel>
+                        </Collapse>
+                    </div>
+                </div>
+                <div>
+                    <h3>Still Have a Question? Ask  support</h3>
+                </div>
+                <div>
+                    <Input placeholder="Name in full" allowClear />
+                    <br />
+                    <br />
+                    <Input placeholder="Enter email " allowClear />
+                    <br />
+                    <br />
+                    <TextArea placeholder="textarea with clear icon" allowClear />
+                    <br />
+                    <br />
+                    <Button type="primary" className='color-primary'>
+                        Send
+                                    </Button>
+                </div>
+            </Content>
+        </Layout>
     )
 }
 
-export default  Support; 
+export default Support;
