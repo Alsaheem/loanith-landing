61982c1a374c5a84c604be2216b1f5f3321caa84
Adebisi Ayomide <Adebisiayomide07@gmail.com>
2020-07-18 21:43:30 +0100

    scrummy

--- src/components/pages/WhyLoanith/WhyLoanith.tsx
+++ src/components/pages/WhyLoanith/WhyLoanith.tsx
@@ -4,7 +4,21 @@ import Layout from "../../common/Layout/Layout";
 const WhyLoanith = () => {
   return (
     <div className="">
-      <h3>Why Loanith</h3>
+      <div className='wrapper-contact'>
+            <div className="banner-contact">
+            </div>
+            <br/>
+            <h3>Light Fast And Powerful</h3>
+            <p>Loanith provides credits with rewards for your every day needs. With the Loanith App, you can earn cash back rewards on a range of purchases while building your credit history.</p>
+            <div className="row">
+              <div className="col-md-6"> <span className='font-weight-bolder'>No Annual Fees:</span>  Off course, that's why we are affordable.</div>
+              <div className="col-md-6"><span className='font-weight-bolder'>Interest free: </span>Get up to 6 months interest free credit.</div>
+              <div className="col-md-6"><span className='font-weight-bolder'>Unlimited Cashback: </span>Up to 1% Cashback on transactions across partner stores.</div>
+              <div className="col-md-6"><span className='font-weight-bolder'>Secured:</span> Approve transactions with your PIN and freeze/unlock your account at will.</div>
+              <div className="col-md-6"><span className='font-weight-bolder'>Cardless Transactions:</span> Guess what! That means no maintenance or replacement fees also.</div>
+              <div className="col-md-6"><span className='font-weight-bolder'>Direct Debit: </span>Enjoy interest free credit for life with our automated repayment feature.</div>
+            </div>
+      </div>
     </div>
   );
 };
